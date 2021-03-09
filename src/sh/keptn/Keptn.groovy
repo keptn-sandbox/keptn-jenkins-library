ffd5d2dfd06707655ba171e5894388ee827a2522
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-09 18:59:39 -0500

    Update Keptn.groovy

--- src/sh/keptn/Keptn.groovy
+++ src/sh/keptn/Keptn.groovy
@@ -581,7 +581,7 @@ def sendStartEvaluationEvent(Map args) {
         |  },
         |  "source": "Jenkins",
         |  "specversion": "1.0",
-        |  "type": "sh.keptn.event.${stage}.delivery.triggered"
+        |  "type": "sh.keptn.event.evaluation.started"
         |}
     """.stripMargin()
 
