aeb349512fe7c5bc22160821e464fe87aa53ac84
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-10 16:26:18 -0500

    Update Keptn.groovy

--- src/sh/keptn/Keptn.groovy
+++ src/sh/keptn/Keptn.groovy
@@ -657,7 +657,7 @@ def waitForEvaluationDoneEvent(Map args) {
                     customHeaders: [[maskValue: true, name: 'x-token', value: "${keptn_api_token}"]], 
                     httpMode: 'GET', 
                     responseHandle: 'STRING', 
-                    url: "${keptn_endpoint}/mongodb-datastore/event?keptnContext=${keptn_context}&type=sh.keptn.event.evaluation.finished", 
+                    url: "${keptn_endpoint}/mongodb-datastore/event?keptncontext=${keptn_context}&type=sh.keptn.event.evaluation.finished", 
                     validResponseCodes: "100:404", 
                     ignoreSslErrors: true
 
