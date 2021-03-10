ca35259e503a4c7da540c16e332997735ef7a100
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-03-10 10:37:44 -0500

    Update Keptn.groovy

--- src/sh/keptn/Keptn.groovy
+++ src/sh/keptn/Keptn.groovy
@@ -113,7 +113,7 @@ def keptnInit(Map args) {
     echo "Project: ${project}"
 
     // Step #0: Generate or use the shipyard file passed
-    def shipyardFileContent = ""apiVersion: "spec.keptn.sh/0.2.0"
+    def shipyardFileContent = """apiVersion: "spec.keptn.sh/0.2.0"
     |  kind: "Shipyard"
     |  metadata:
     |    name: "shipyard-quality-gates"
@@ -127,7 +127,7 @@ def keptnInit(Map args) {
     |                properties:
     |                  teststrategy: "performance"
     |              - name: "evaluation" 
-    "".stripMargin()
+    """.stripMargin()
     if (args.containsKey("shipyard")) {
         // lets see if a shipyard was passed - if so - we use that shipyard.yaml
         shipyardFileContent = readFile(args.shipyard)
